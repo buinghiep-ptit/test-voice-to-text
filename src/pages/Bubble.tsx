c6a7433a85d91abbc1d807b034dae0bb194a0b32
Bùi Nghiệp <nghiepbv2@fpt.com.vn>
2025-04-03 20:34:40 +0700

    update timeout hidden greeting text

--- src/pages/Bubble.tsx
+++ src/pages/Bubble.tsx
@@ -1,23 +1,9 @@
+import { useEffect, useState } from "react";
 import Typewriter from "../components/TypeWriter";
 
-// .chat-toggle {
-//   display: flex;
-//   position: fixed;
-//   bottom: 24px;
-//   right: 6px;
-//   gap: 10px;
-//   align-items: flex-start;
-// }
-
-// .chat-toggle .greeting {
-//   background: rgb(32, 59, 220);
-//   padding: 0.625rem 0.625rem 0.625rem 0.75rem;
-//   border-radius: 30px;
-//   animation: shake 1.6s ease 0s infinite normal none running;
-//   cursor: pointer;
-// }
-
 function Bubble() {
+  const [isShowSayHi, setIsShowSwayHi] = useState(true);
+
   const openChat = () => {
     window.parent.postMessage(
       {
@@ -30,14 +16,22 @@ function Bubble() {
     );
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShowSwayHi(false);
+    }, 5000);
+  }, []);
+
   return (
     <div className="chat-toggle bg-transparent flex items-start justify-end overflow-hidden gap-1 cursor-pointer">
-      <div
-        className="greeting bg-[#0F62FE] py-2! px-3! rounded-3xl rounded-br-none"
-        onMouseDown={openChat}
-      >
-        <Typewriter text="Chang luôn ở đây để giúp bạn bạn nhé" speed={50} />
-      </div>
+      {isShowSayHi && (
+        <div
+          className="greeting bg-[#0F62FE] py-2! px-3! rounded-3xl rounded-br-none"
+          onMouseDown={openChat}
+        >
+          <Typewriter text="Chang luôn ở đây để giúp bạn bạn nhé" speed={50} />
+        </div>
+      )}
       <img
         src="/ai-agent/sdk/assets/images/bot-icon-blue-unscreen.gif"
         alt="ic"
