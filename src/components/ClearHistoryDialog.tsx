f99cb573159f9469c764b5058634f5d242fa43d3
HUNG (MIKE) <hungtd29@aiaracorp.com>
2025-10-15 15:38:50 +0700

    fix: update clear content modal

--- src/components/ClearHistoryDialog.tsx
+++ src/components/ClearHistoryDialog.tsx
@@ -5,24 +5,28 @@ interface Props {
 
 const ClearHistoryDialog = ({ cancelClear, confirmClear }: Props) => {
   return (
-    <div className="fixed inset-0 flex items-center bg-black/50 justify-center z-1000! p-4!">
+    <div className="modal-overlay" onClick={cancelClear}>
       <div className="bg-white rounded-xl p-6! max-w-sm w-full shadow-xl transform transition-all animate-fade-in">
-        <h3 className="font-semibold! mb-3!">Xoá lịch sử trò chuyện?</h3>
+        <h3 className="modal-title font-semibold! mb-6!">
+          Làm mới phiên trò chuyện?
+        </h3>
         <p className="mb-5! text-sm!">
-          Bạn có chắc chắn muốn xoá lịch sử trò chuyện không?
+          Chang sẽ quên các thông tin đã trao đổi trước đó và sẽ chạy lại.{" "}
+          <br /> Vui lòng hỏi lại câu hỏi để nhận được câu trả lời chính xác dựa
+          trên dữ liệu mới. Bạn có muốn làm mới không?
         </p>
         <div className="flex justify-end space-x-3!">
           <button
             onClick={cancelClear}
             className="cursor-pointer px-4! py-2! rounded-xl bg-gray-100 hover:bg-gray-200 transition-colors"
           >
-            Hủy
+            Không
           </button>
           <button
             onClick={confirmClear}
-            className="cursor-pointer px-4! py-2! rounded-xl text-white bg-red-500 hover:bg-red-600 transition-colors"
+            className="btn-submit cursor-pointer px-4! py-2! !rounded-xl text-white transition-colors"
           >
-            Xoá
+            Có
           </button>
         </div>
       </div>
